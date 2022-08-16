604c10b04c0f017606c8845ec18739523705941e
zeuselijah <simeonhendricks@gmail.com>
2022-08-16 14:36:44 -0400

    wip: adding second api

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 var url = "https://api.quotable.io/random";
-
+const url2 ="https://picsum.photos/200/300?random=1";
 
 
 
 
 $("#quoteie").click(function(){
+    // $.ajax({
+    //     dataType: 'json',
+    //     url: url,
+    //     success: function(data){
+    //         console.log(data);
+    //         document.getElementById("quoteText").innerHTML = data['content'];
+    //         document.getElementById("arthur").innerHTML = data['author'];
+    //     }
+    // })
     $.ajax({
         dataType: 'json',
-        url: url,
-        success: function(data){
-            console.log(data);
+        url: url2,
+        success: function(data2){
+            console.log(data2);
         }
     })
 })
