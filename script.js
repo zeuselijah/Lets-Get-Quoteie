f8e0323fbf58822e24c6ed6afb2f9d3062089fd2
zeuselijah <simeonhendricks@gmail.com>
2022-08-17 11:58:58 -0400

    wip: adds background image and simplified functionality

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 var url = "https://api.quotable.io/random";
-const url2 ="https://picsum.photos/200/300?random=1";
+const url2 ="https://picsum.photos/200/300";
 
 
+$(document).ready(getQuote);
 
 
-$("#quoteie").click(function(){
-    // $.ajax({
-    //     dataType: 'json',
-    //     url: url,
-    //     success: function(data){
-    //         console.log(data);
-    //         document.getElementById("quoteText").innerHTML = data['content'];
-    //         document.getElementById("arthur").innerHTML = data['author'];
-    //     }
-    // })
+$("#quoteie").click(getQuote);
+
+function getQuote(){
     $.ajax({
         dataType: 'json',
-        url: url2,
-        success: function(data2){
-            console.log(data2);
+        url: url,
+        success: function(data){
+            console.log(data);
+            $("#quoteText").html(data['content']);
+            $("#arthur").html(data['author']);
+            changeBackgroundImage();  
         }
     })
-})
+};
 
-
-
+function changeBackgroundImage(){
+    document.body.style.backgroundImage = "url('https://picsum.photos/200/300?random=1')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    console.log("background");
+}
 
 
 
